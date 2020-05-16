0ad4fd69b15e8fea01ce2e647f738735b533cb13
xiateserenity <xiateserenity@gmail.com>
2020-05-16 01:30:50 -0500

    Added beginning of drawer functionality

--- App.js
+++ App.js
@@ -1,19 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, View, Text, TouchableOpacity } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+function DrawerMenu(props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={{
+        justifyContent: 'flex-end',
+        height: 200,
+        backgroundColor: '#363636',
+      }}
+    >
+      <Text style={{ color: 'white', paddingHorizontal: 20, fontSize: 20 }}>
+        Founder's Third
+      </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingHorizontal: 15,
+          paddingTop: 10,
+          paddingBottom: 20,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#242424',
+            borderRadius: 20,
+            paddingHorizontal: 10,
+            paddingVertical: 5,
+          }}
+        >
+          <Text style={{ color: '#fff', fontWeight: 'bold' }}>14 members</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#242424',
+            borderRadius: 20,
+            paddingHorizontal: 10,
+            paddingVertical: 5,
+          }}
+        >
+          <Text style={{ color: '#fff', fontWeight: 'bold' }}>Invite +</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function HomeScreen() {
+  return (
+    <View
+      style={{
+        backgroundColor: '#242424',
+        flex: 1,
+      }}
+    >
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName='Home'
+        edgeWidth={100}
+        drawerStyle={{
+          backgroundColor: '#242424',
+          width: 350,
+        }}
+        drawerContent={(props) => <DrawerMenu {...props} />}
+      >
+        <Drawer.Screen name='HomeScreen' component={HomeScreen}></Drawer.Screen>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
